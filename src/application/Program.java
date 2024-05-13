18c8b42807fddd926c99dfa80aae1be044275f52
Guilherme da Silva Boava <guiboava@hotmail.com>
2024-05-13 13:41:22 -0300

    Department and Seller Class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dep = new Department(1, "Books");
+		
+		System.out.println(dep);
+
+	}
+
+}
