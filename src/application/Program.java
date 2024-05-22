d8706a16bee0efd6c79e864670a7553829bd3672
Guilherme da Silva Boava <guiboava@hotmail.com>
2024-05-22 17:52:52 -0300

    Delete Implememetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.dao.impl.SellerDaoJDBC;
@@ -26,21 +28,27 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
 		System.out.println("\n=== TEST 3: Seller findByAll =====");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
 		System.out.println("\n=== TEST 4: Seller Insert =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
 		System.out.println("\n=== TEST 5: Seller Update =====");
 		seller = sellerDao.findById(1);
 		seller.setName("Bob Brown");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
-
+		
+		System.out.println("\n=== TEST 6: Seller Delete =====");
+		sellerDao.deleteById(Integer.parseInt(JOptionPane.showInputDialog("Digite o id do seller a ser deletado: ")));
+		System.out.println("Usar deleted sucessfull!");
 	}
 }
